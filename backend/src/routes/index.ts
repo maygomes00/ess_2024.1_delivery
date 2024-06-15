be36205023618b1b50ba400afea93194669a2346
undecillion <86275510+undecillion@users.noreply.github.com>
2024-06-15 07:19:03 -0300

    feat: Add restaurant registration functionality
    (pre-testing)

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Express } from 'express';
-import restaurantRouter from './restaurant.routes';
+import { restaurantsRouter } from './restaurant.routes';
 import menuRouter from './menu.routes';
 import categoryRouter from './category.routes';
 import userRoutes from './user.routes'
 import itemRouter from './item.routes';
 
 const setupRoutes = (app: Express): void => {
-  app.use('/restaurant', restaurantRouter);
+  app.use('/restaurant', restaurantsRouter);
   app.use('/restaurant/menu', menuRouter);
   app.use('/restaurant/menu/category', categoryRouter);
   app.use('/users', userRoutes);
