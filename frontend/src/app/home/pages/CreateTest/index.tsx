23ac7732370faf6ee84edd72de91a564ff0e24f6
Cafeko <caiofgs2329@gmail.com>
2024-07-05 09:06:41 -0300

    feat: frontend MainContext

--- frontend/src/app/home/pages/CreateTest/index.tsx
+++ frontend/src/app/home/pages/CreateTest/index.tsx
@@ -1,64 +1,18 @@
-import { SubmitHandler, useForm } from "react-hook-form";
 import styles from "./index.module.css";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useContext, useEffect } from "react";
-import { HomeContext } from "../../context/HomeContext";
-import { TestFormSchema, TestFormType } from "../../forms/TestForm";
+import { useContext } from "react";
+import { MainContext } from "../../context/MainContext";
 import { Link } from "react-router-dom";
-import Button from "../../../../shared/components/Button";
 
 const CreateTest = () => {
-  const { state, prevState, service } = useContext(HomeContext);
-
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<TestFormType>({
-    resolver: zodResolver(TestFormSchema),
-  });
-
-  const onSubmit: SubmitHandler<TestFormType> = async (body) => {
-    service.createTest(body);
-    reset();
-  };
-
-  useEffect(() => {
-    if (
-      state.createTestRequestStatus !== prevState?.createTestRequestStatus &&
-      state.createTestRequestStatus.isSuccess()
-    ) {
-      alert("Teste criado com sucesso!");
-    }
-  }, [state, prevState]);
+  const userContext = useContext(MainContext).user
+  const [id, setId] = userContext.id
 
   return (
     <section className={styles.container}>
       <h1 className={styles.title}>Crie um test</h1>
-      <form className={styles.formContainer} onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles.formInputContainer}>
-          <input
-            data-cy="input-name"
-            {...register("name")}
-            placeholder="Digite o nome"
-            className={styles.formInput}
-          />
-          {errors.name && (
-            <span data-cy="input-name-error" className={styles.formError}>
-              {errors.name.message}
-            </span>
-          )}
-        </div>
-
-        <Button data-cy="create" type="submit">
-          CRIAR
-        </Button>
-
-        <Link data-cy="view-tests" to="/tests">
-          VER TESTS
-        </Link>
-      </form>
+      <button onClick={() => setId("12345")}>AAAAAA</button>
+      <p>{id}</p>
+      <Link to="/tests">AAAAAAAA2</Link>
     </section>
   );
 };
