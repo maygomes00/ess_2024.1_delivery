fd7bf98777ea8cd85595ecdc66a0a1c141d909e6
agsa2 <agsa0108@gmail.com>
2024-07-06 14:06:27 -0300

    feat:item's active restriction added to category

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -1,25 +1,23 @@
-// src/types.ts
-
 export type Item = {
-    id: string;
-    restauranteId: string;
-    nome: string;
-    preco: string;
-    descricao: string;
-    categorias: string[];
-    imagem: string;
-  };
-  
-  export type ItemData = {
-    itens: Item[];
-  };
-  
-  export type Category = {
-    id: string;
-    nome: string;
-  };
-  
-  export type CategoryData = {
-    categorias: Category[];
-  };
-  
+  id: string;
+  active: string;
+  restaurantId: string; 
+  nome: string; 
+  preco: string;
+  descricao: string;
+  categorias: string;
+  image: string;
+};
+
+export type ItemData = {
+  itens: Item[];
+};
+
+export type Category = {
+  id: string;
+  nome: string;
+};
+
+export type CategoryData = {
+  categorias: Category[];
+};
