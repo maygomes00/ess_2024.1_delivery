5aa1825bce1bf08c4a271bda739474347b278bdc
Cafeko <caiofgs2329@gmail.com>
2024-07-09 10:27:14 -0300

    Add: SelectorButton

--- frontend/src/shared/components/PageBlock/index.tsx
+++ frontend/src/shared/components/PageBlock/index.tsx
@@ -9,15 +9,16 @@ type PageBlockProps = {
 }
 
 /*
-  Recebe os elementos que devem ser exibidos denro do bloco em uma lista e recebe um indice, 
-  exibe no bloco apenas o elemento correspondente ao indice recebido. 
+  Recebe os elementos que devem ser exibidos dentro do bloco em uma lista e recebe um indice, 
+  exibe no bloco apenas o elemento correspondente ao indice recebido.
 */
-const PageBlock = ({elements, index = -1, border = 2, border_color = "black"}: PageBlockProps) => {
+const PageBlock = ({elements = [], index = -1, border = 2, border_color = "black"}: PageBlockProps) => {
   const linhaStyle = {
     "--border": `${border}px`,
     "--border_color": border_color
   } as React.CSSProperties
 
+  // Retorna o elemento correspondente ao indice, ou nulo caso o indice não corresponda a nenhum elemento.
   function select_element(elements_list, element_index : number) {
     if ((element_index >= 0) && (element_index < elements_list.length)) {
       return elements_list[element_index]
