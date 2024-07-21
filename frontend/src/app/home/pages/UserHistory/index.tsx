c39b955e7b6dd5e54bca3a0b5425fde2508d3bbc
Daniel Cavalcante <dvictor.cintra@gmail.com>
2024-07-21 11:22:20 -0300

    style: update useer history page visuals

--- frontend/src/app/home/pages/UserHistory/index.tsx
+++ frontend/src/app/home/pages/UserHistory/index.tsx
@@ -1,13 +1,14 @@
+import styles from "./index.module.css";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 import { getUserOrders } from "../../../../shared/services/userService";
 import { getItemDetails } from "../../../../shared/services/ItensService";
-
-import { Pedido } from "../../../../shared/types/User";
+import { Pedido, UserItem } from "../../../../shared/types/User";
 import { Item } from "../../../../shared/types/types";
-
-import styles from "./index.module.css";
+import restaurantProfile from "../../../../shared/assets/default_restaurant_pfp.svg";
 
 const UserHistory = () => {
   const { user_id } = useParams<{ user_id: string }>();
@@ -46,6 +47,32 @@ const UserHistory = () => {
     fetchOrders();
   }, [user_id]);
 
+  const capitalizeFirstLetter = (string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const formattedDate = format(date, "EEE dd MMMM yyyy", { locale: ptBR });
+    return capitalizeFirstLetter(formattedDate);
+  };
+
+  const groupItemsByRestaurant = (userItems: UserItem[]) => {
+    const groupedItems: { [key: string]: UserItem[] } = {};
+
+    userItems.forEach((userItem) => {
+      const item = items[userItem.produto_id];
+      if (item) {
+        if (!groupedItems[item.restaurant_id]) {
+          groupedItems[item.restaurant_id] = [];
+        }
+        groupedItems[item.restaurant_id].push(userItem);
+      }
+    });
+
+    return groupedItems;
+  };
+
   if (error) {
     return <div>{error}</div>;
   }
@@ -55,26 +82,41 @@ const UserHistory = () => {
       <h1>Histórico</h1>
       {orders.map((order) => (
         <div className={styles.order}>
-          <p>Data: {order.data}</p>
+          <p>{formatDate(order.data)}</p>
           <div key={order.order_id} className={styles.orderBox}>
-            <h3>Pedido ID: {order.order_id}</h3>
-            <h4>Itens:</h4>
-            <ul className={styles.list}>
-              {order.itens.map((userItem) => {
-                const item = items[userItem.produto_id];
-                return item ? (
-                  <li key={userItem.produto_id}>
-                    {item.name} - Quantidade: {userItem.quantity} - Preço:{" "}
-                    {item.price}
+            {/* <h3>Pedido concluido - N° {order.order_id}</h3> */}
+            <ul className={styles.itemsInfo}>
+              {Object.entries(groupItemsByRestaurant(order.itens)).map(
+                ([restaurantId, userItems]) => (
+                  <li key={restaurantId} className={styles.list}>
+                    <div className={styles.listItemHeader}>
+                      <img
+                        src={restaurantProfile}
+                        alt="restaurant"
+                        className={styles.restaurantImage}
+                      />
+                      <h3>Restaurante {restaurantId}</h3>
+                    </div>
+                    <ul>
+                      {userItems.map((userItem) => {
+                        const item = items[userItem.produto_id];
+                        return item ? (
+                          <li key={userItem.produto_id} className={styles.list}>
+                            <strong>{userItem.quantity}</strong> {item.name} -
+                            Preço: {item.price}
+                          </li>
+                        ) : (
+                          <li key={userItem.produto_id} className={styles.list}>
+                            Carregando detalhes do item...
+                          </li>
+                        );
+                      })}
+                    </ul>
                   </li>
-                ) : (
-                  <li key={userItem.produto_id}>
-                    Carregando detalhes do item...
-                  </li>
-                );
-              })}
+                )
+              )}
             </ul>
-            <p>Total: {order.total}</p>
+            {/* <p>Total: {order.total}</p> */}
           </div>
         </div>
       ))}
