9bfde246a0fdab613bf83ee3059d69f48bb1d2de
Mayara Gomes <mayaragomes@MacBook-Air-de-Mayara.local>
2024-07-22 02:09:27 -0300

    fix: alterando algumas linhas do global.css para adequar as páginas de Login

--- frontend/cypress/e2e/features/common-step-definitions/1.ts
+++ frontend/cypress/e2e/features/common-step-definitions/1.ts
@@ -1,1 +1,6 @@
 // e.g Given("o usuário "tal" está logado", (page: string) => {});
+import { Given } from 'cypress-cucumber-preprocessor/steps';
+
+Given("o usuário está na página {string}", (page) => {
+  cy.visit(`/${page}`);
+});
